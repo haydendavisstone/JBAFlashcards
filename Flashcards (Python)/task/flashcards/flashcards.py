2ba430eba9d63050b89ca86926cb499597d92cc3
haydendavisstone <haydendavisstone@gmail.com>
2023-03-15 12:33:46 -0400

    Added functionality to forbid duplicate term/def.

--- Flashcards (Python)/task/flashcards/flashcards.py
+++ Flashcards (Python)/task/flashcards/flashcards.py
@@ -3,18 +3,34 @@ class Flashcard:
         self.term = term
         self.definition = definition
 
-    def output(self, answer):
-        if answer == self.definition:
-            print('Correct!')
-        else:
-            print(f'Wrong. The right answer is "{self.definition}".')
 
 def add_new_card(flashcards):
     num_cards = input('Input the number of cards:\n')
 
     for i in range(1, int(num_cards)+1):
+
         term = input(f'The term for card #{i}:\n')
+        while True:
+            try:
+                if term not in [i.term for i in flashcards]:
+                    break
+                else:
+                    term = input(f'The term "{term}" already exists. Try again:\n')
+            except ValueError:
+                print("Provide an valid term for card...")
+                continue
+
         definition = input(f'The definition for card #{i}:\n')
+        while True:
+            try:
+                if definition not in [i.definition for i in flashcards]:
+                    break
+                else:
+                    definition = input(f'The definition "{definition}" already exists. Try again:\n')
+            except ValueError:
+                print("Provide an valid definition for card...")
+                continue
+
         flashcards.append(Flashcard(term, definition))
 
 
@@ -25,7 +41,15 @@ def main():
 
     for flashcard in flashcards:
         answer = input(f'Print the definition of "{flashcard.term}":\n')
-        flashcard.output(answer)
+
+        if answer == flashcard.definition:
+            print('Correct!')
+        elif answer in [i.definition for i in flashcards]:
+            correct_term = [i.term for i in flashcards if i.definition == answer]
+            correct_term = correct_term[0]
+            print(f'Wrong. The right answer is "{flashcard.definition}", but your definition is correct for "{correct_term}".')
+        else:
+            print(f'Wrong. The right answer is "{flashcard.definition}".')
 
 
 if __name__ == '__main__':
